fcc068afa45eae8424ac1fd7e0c6ad5cfc482d12
jan <janmehr93@gmx.de>
2017-11-14 08:57:10 +0100

    overflow menu für toolbar +  xml/java für profile + übergang home zu profile

--- app/src/main/java/com/example/jan/t9_mobileapp/Homescreen.java
+++ app/src/main/java/com/example/jan/t9_mobileapp/Homescreen.java
@@ -1,9 +1,13 @@
 package com.example.jan.t9_mobileapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Homescreen extends AppCompatActivity {
 
@@ -11,9 +15,24 @@ public class Homescreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar2);
 
+        Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
-        setTitle(R.string.title_homescreen);
+
+        Button btn_profile = (Button) findViewById(R.id.button);
+        btn_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Homescreen.this, Profile.class));
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+
 }
