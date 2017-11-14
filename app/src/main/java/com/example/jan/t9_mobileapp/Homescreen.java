bff154b70792ef9a03130c65f9c3abe5d31e2365
jan <janmehr93@gmx.de>
2017-11-14 05:46:45 +0100

    erste tollbar, drawables und style zu homescreen hinzugefügt

--- app/src/main/java/com/example/jan/t9_mobileapp/Homescreen.java
+++ app/src/main/java/com/example/jan/t9_mobileapp/Homescreen.java
@@ -2,6 +2,8 @@ package com.example.jan.t9_mobileapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class Homescreen extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Homescreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar2);
+
+        setSupportActionBar(myToolbar);
+        setTitle(R.string.title_homescreen);
     }
 }
