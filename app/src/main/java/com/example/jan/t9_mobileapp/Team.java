3adc43b5bd97d48541896aac2ed3d0bfb08db3d7
taraszaika <taraszajika@gmail.com>
2017-11-29 10:47:15 +0100

    strings

--- app/src/main/java/com/example/jan/t9_mobileapp/Team.java
+++ app/src/main/java/com/example/jan/t9_mobileapp/Team.java
@@ -1,17 +1,16 @@
 package com.example.jan.t9_mobileapp;
 
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 
 /**
